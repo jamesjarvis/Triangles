d9bb585e4ca6bb5eb6fadfdb0d2054a008d61316
James Jarvis <jj333@kent.ac.uk>
2018-01-30 16:52:47 +0000

    Writing some important comment in the Main class

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,15 @@
 public class Main {
 
-    public static void main(String[] args){
+    public int SIZE = 600;
+    public int GRAPH_SIZE = 24;
+    /*
+    Of course, the grpah being created would work from 0-24, with 0,0 being positioned in the bottom left
+    But the graphics will seemingly start from the top left, which means all y values have to be inverted
+    And the values will have to be scaled up to the size of the window (I will not bother creating a scale,
+    but maybe some grid lines)
+     */
 
+    public static void main(String[] args){
+        
     }
 }
